416bbfcb258f1cae8bc099a2e7d534fcd8cc97ad
jordanrj <jordanrjones33@gmail.com>
2018-04-25 21:08:38 -0700

    Fixed ajax request, and converted to angular.  Need to process response.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var app = angular.module("chartApp", ['chart.js']);
-app.controller("chartController", function($scope) {
+app.controller("chartController", function($scope, $http) {
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
-  $scope.data = //[
-    //[65, 59, 80, 81, 56, 55, 40]
-    //[28, 48, 40, 19, 86, 27, 90]
-  //];
+  
+  /* gets data from tsa.gov source;  converts to json using sheetjs */
+  $http({
+    url: "https://cors-anywhere.herokuapp.com/https://www.dhs.gov/sites/default/files/publications/claims-2010-2013_0.xls",
+    method: "GET",
+    responseType: "arraybuffer"
+  })
+  .then(function(res) {
+    var data = new Uint8Array(res.data);
+    $scope.data = [];
+    //using xlsx and workbooks
+    var workbook = XLSX.read(data, {type: 'array'});
+    var sheet = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[sheet];
+    for (let i = 0; i < 10; i++) {
+      $scope.data.push(XLSX.utils.sheet_to_json(worksheet)[i]);
+    }
+    console.log($scope.data);
+  });
+
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
@@ -29,25 +45,3 @@ app.controller("chartController", function($scope) {
     }
   };
 });
-
-var fileName = "claims.xls";
-
-/*function readFile(file) {
-    var reader = new FileReader();
-    reader.onlead = readSuccess;
-    function readSuccess(e) {
-        console.log("skjdflsdfjlksdjflksdjflk");
-    };
-
-    reader.readAsText(file);
-};
-
-readFile(fileName);*/
-
-var reader= new XMLHttpRequest();
-reader.open('GET', 'https://www.dhs.gov/sites/default/files/publications/claims-2010-2013_0.xls');
-reader.onreadystatechange = readSuccess();
-function readSuccess(evt) {                                             
-   console.log(reader.responseText);                                
-};
-reader.send();
